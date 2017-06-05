27620d8d49b1f36f243c92f5e8e3220ed2aad8d4
Harry Devine <lifter@comcast.net>
2017-06-05 09:54:26 -0400

    Updated game note message

--- gameContent.php
+++ gameContent.php
@@ -2,8 +2,8 @@
 $content = "";
 
 $content = $content . "<div class=\"maniacsCenter\">\n";
-$content = $content . "  <div class=\"maniacsWelcome\">2016/2017 South Jersey Maniacs 18U Tournament Schedule</div>\n";
-$content = $content . "  <div class=\"maniacsCopyright\">Note: Tournaments listed here will not show individual details, such as final score.  For that informationn, use the GameChanger control located at the bottom of the website.<hr>\n";
+$content = $content . "  <div class=\"maniacsWelcome\">2016/2017 South Jersey Maniacs 18U Game Schedule</div>\n";
+$content = $content . "  <div class=\"maniacsCopyright\">Note: Scores for each game will be shown here, but full details can be found by using the GameChanger control located at the bottom of the website.<hr>\n";
 $content = $content . "    <div id=\"gameGrid\"></div>\n";
 $content = $content . "</div>\n";
 
